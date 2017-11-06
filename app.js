5539a1c1d7ec5782910aeb7159b32901b60f28d6
Katy Eng <katye610@gmail.com>
2017-11-06 18:36:27 -0500

    Chalk, nodemon script, app.use added

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const chalk = require('chalk');
+
+app.use('/', function(req, res, next){
+  console.log(`Method: ${req.method}`)
+  console.log(`Method: ${req.path}`)
+  next()
+})
 
 app.get('/', function(req, res, next){
-  res.send('Welcome to FakeTwitter!');
+  res.send('Welcome to FakeTwitter!')
 });
 
 app.listen(3000, function(){
-  console.log('Server listening on Port 3000!')
+  console.log(chalk.blue('Server listening on Port 3000!'))
 });
