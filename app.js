5c192a319aed5a75cc2df9d3e3b8ae0ab1b52fe4
Katy Eng <katye610@gmail.com>
2017-11-07 11:13:37 -0500

    Made tweetBank and set up lodash and nunjucks

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+const morgan = require('morgan');
 
-app.use('/', function(req, res, next){
-  console.log(`Method: ${req.method}`)
-  console.log(`Method: ${req.path}`)
-  next()
-})
+var locals = {
+  title: 'An Example',
+  people: [
+      { name: 'Gandalf'},
+      { name: 'Frodo' },
+      { name: 'Hermione'}
+  ]
+};
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+nunjucks.render('index.html', locals, function (err, output) {
+  console.log(output);
+}); //Lines 17-19 are what logs the template contents of index.html to our terminal
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.use(morgan('dev'))
+// The following is the equivalent of morgan('dev'):
+//  function(req, res, next){
+//   console.log(method + path + status + responseTime + content)
+//   next()
+// })
 
 app.get('/', function(req, res, next){
-  res.send('Welcome to FakeTwitter!')
+  // res.send('Welcome to FakeTwitter!')
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(3000, function(){
